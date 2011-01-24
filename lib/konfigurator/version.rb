32b2975599ff4dc2855e62bcf0b5959a45196e9c
Chris Kowalik <chris@nu7hat.ch>
2011-01-24 18:11:46 +0100

    Version bump to 0.2.0

--- lib/konfigurator/version.rb
+++ lib/konfigurator/version.rb
@@ -1,8 +1,8 @@
 module Konfigurator
   module Version # :nodoc:
     MAJOR  = 0
-    MINOR  = 1
-    PATCH  = 1
+    MINOR  = 2
+    PATCH  = 0
     STRING = [MAJOR, MINOR, PATCH].join('.')
   end
   
